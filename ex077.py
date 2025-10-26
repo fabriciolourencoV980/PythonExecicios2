28947fd7465a4a4cde3fe921c111d093a79f4365
Fabrício Lourenco da Silva <lourencofabricio824@gmail.com>
2025-10-26 16:04:19 -0300

    .

--- ex077.py
+++ ex077.py
@@ -3,4 +3,12 @@ Crie um programa que tenha uma tupla com várias palavras, depois disso, você d
 palavra, quais são as suas vogais
 """
 
+palavras = ('aprender', 'programar', 'linguagem', 'python',
+            'curso', 'gratis', 'estudante', 'praticar',
+            'trabalhador', 'futuro','mercado','programador')
 
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
